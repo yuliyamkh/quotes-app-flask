0cbe5aa3d76d8214ad8d94bc6cac98da588698f5
yuliyamkh <yuliya.mkhayan@gmail.com>
2024-03-04 21:19:11 +0100

    Fix error with requests

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ def get_quotes_for_category():
     return render_template('index.html', category=CATEGORIES, quotes=quotes)
 
 
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
